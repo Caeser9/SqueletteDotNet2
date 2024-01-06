539cace68232f8b642d1365207b73647dfa54709
Caeser9 <kaycerkhouini86@outlook.fr>
2024-01-06 11:17:32 +0100

    first commit

---
+++ Examen.Infrastructure/ExamenContext.cs
@@ -0,0 +1,38 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.Infrastructure
+{
+    public class ExamenContext:DbContext
+    {
+      
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies(); 
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                       Initial Catalog= Examen;
+                       Integrated Security=true;MultipleActiveResultSets=true");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+          
+ 
+
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            
+        }
+    }
+}
